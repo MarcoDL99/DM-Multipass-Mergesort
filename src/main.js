eeb084573ab34a2ea5cf2cfbe572fcfe7df888d1
MarcoDL99 <marcodl722@gmail.com>
2022-12-16 23:32:07 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ let options =
 }
 let maxPerLine = 10
 
-let relation, frames, runs, currentFrames, currentFramesObject;
+let relation, frames, runs, currentFrames, currentFramesObject, currentRunsObject;
 let currentRun = 0;
 runs = [[]];
 let started = false;
@@ -102,16 +102,16 @@ function start() {
         started = true
 
         //generateRelation()
-        //createRuns()
         //cameraMover.rotateToBuffer()
         createFrames()
         createRelation()
     }
     else {
         movePagesToFrame(function () {
-            moveRelationToFrames((function () {
-                updateFrames()
-            }))
+            //setupRelationScan((function () {
+            //updateFrames()
+            setupCreateRuns()
+            //}))
 
         })
 
@@ -122,7 +122,7 @@ function updateFrames() {         //  create a loop function
     setTimeout(function () {   //  call a 1s setTimeout when the loop is called
         let index = Math.floor(Math.random() * (options.frames - 1));
         console.log(options)
-        if (options.pages - options.frames <0) {
+        if (options.pages - options.frames < 0) {
             while (index < (options.frames - options.pages - 1)) {
                 index = Math.floor(Math.random() * (options.frames - 1));
             }
@@ -172,32 +172,34 @@ function createFrames() {
     });
 
     const geometry = new THREE.BoxGeometry(size, size, size);
-    for (let z = 0; z < options.frames - 1; z++) {
+    for (let z = 0; z < options.frames; z++) {
 
         const mesh = new THREE.Mesh(geometry, material);
         const edges = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.MeshPhongMaterial({
-            emissive: 0xffffff,
+            color: 0x000000,
+            emissive: 0x000000,
             side: THREE.DoubleSide,
 
         }));
         mesh.add(edges)
         mesh.position.set(0.0, 0.0, 2 + 1.5 * size * z);
-        mesh.castShadow = true;
+        
         frames.add(mesh);
-    }
+    }/*
     let mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({
         color: 0xffae00,
         side: THREE.DoubleSide,
     }));
     let edges = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.MeshPhongMaterial({
-        emissive: 0xffffff,
+        color: 0x000000,
+        emissive: 0x000000,
         side: THREE.DoubleSide,
 
     }));
     mesh.add(edges)
     mesh.position.set(0.0, 0.0, 2 + 1.5 * size * (options.frames - 1));
-    mesh.castShadow = true;
-    frames.add(mesh);
+    
+    frames.add(mesh);*/
     scene.add(frames);
 }
 function createRelation() {
@@ -231,7 +233,8 @@ function createRelation() {
         }
         const mesh = new THREE.Mesh(geometry, material);
         const edges = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.MeshPhongMaterial({
-            emissive: 0xffffff,
+            color: 0x000000,
+            emissive: 0x000000,
             side: THREE.DoubleSide,
 
         }));
@@ -240,7 +243,7 @@ function createRelation() {
         finalPositions.push(new THREE.Vector3(1.1 * size * (maxPerLine - x), y, 0.0))
         // mesh.position.set(2 + 1.1 * size * x, y, 0);
         mesh.position.set(0, 0, 0)
-        mesh.castShadow = true;
+        
         relation.add(mesh);
         x++
     }
@@ -264,7 +267,7 @@ function createRelation() {
     relation.name = "relation"
     scene.add(relation);
 }
-function moveRelationToFrames(callback) {
+function setupRelationScan(callback) {  //Step i (?)
     currentFramesObject = new THREE.Group();
     const material = new THREE.MeshPhongMaterial({
         color: 0x00ff00,
@@ -275,14 +278,15 @@ function moveRelationToFrames(callback) {
     for (let z = 0; z < options.frames - 1; z++) {
         const mesh = new THREE.Mesh(geometry, material);
         const edges = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.MeshPhongMaterial({
-            emissive: 0xffffff,
+            color: 0x000000,
+            emissive: 0x000000,
             side: THREE.DoubleSide,
 
         }));
         mesh.add(edges)
         mesh.position.set(0.0, (currentFrames[z] - 1.0) / 2, 2 + 1.5 * size * z);
         mesh.scale.set(1, currentFrames[z], 1)
-        mesh.castShadow = true;
+        
         currentFramesObject.add(mesh);
     }
     let mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({
@@ -290,17 +294,87 @@ function moveRelationToFrames(callback) {
         side: THREE.DoubleSide,
     }))
     const edges = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.MeshPhongMaterial({
-        emissive: 0xffffff,
+        color: 0x000000,
+        emissive: 0x000000,
         side: THREE.DoubleSide,
 
     }));
     mesh.add(edges)
     mesh.position.set(0.0, 0.0, 2 + 1.5 * size * (options.frames - 1));
     mesh.scale.set(1, currentFrames[currentFrames.length - 1], 1)
-    mesh.castShadow = true;
+    
     currentFramesObject.add(mesh);
     scene.add(currentFramesObject);
     callback()
+}
+function setupCreateRuns() { //Step 0
+    currentFramesObject = new THREE.Group();
+    const material = new THREE.MeshPhongMaterial({
+        color: 0x00ff00,
+        side: THREE.DoubleSide,
+    });
+
+    const geometry = new THREE.BoxGeometry(size, size, size);
+    for (let z = 0; z < options.frames; z++) {
+        const mesh = new THREE.Mesh(geometry, material);
+        const edges = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.MeshPhongMaterial({
+            color: 0x000000,
+            emissive: 0x000000,
+            side: THREE.DoubleSide,
+
+        }));
+        mesh.add(edges)
+        mesh.position.set(0.0, (currentFrames[z] - 1.0) / 2, 2 + 1.5 * size * z);
+        mesh.scale.set(1, currentFrames[z], 1)
+        
+        currentFramesObject.add(mesh);
+    }
+    scene.add(currentFramesObject);
+    currentRunsObject = new THREE.Group();
+    for (let i = 0; i < options.pages / options.frames; i++) {
+        const mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            side: THREE.DoubleSide
+        }))
+        const edges = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), new THREE.MeshPhongMaterial({
+            color: 0x000000,
+            emissive: 0x000000,
+            side: THREE.DoubleSide,
+
+        }));
+    }
+    createRuns()
+}
+function createRuns() {         //STEP 0
+    setTimeout(function () {   //  call a 0.15s setTimeout when the loop is called
+        let index = Math.floor(Math.random() * (options.frames));
+        let fullFrames = 0;
+        for (let i = 0; i < options.frames; i++) {
+            if (currentFrames[i] > 0.95) {
+                fullFrames++
+            }
+        }
+        if (fullFrames == options.frames) {
+            console.log("FULL")
+            for (let i = 0; i < options.frames; i++) {
+
+                currentFrames[i] = 1.0
+                currentFramesObject.children[i].scale.set(1, currentFrames[i], 1)
+                currentFramesObject.children[i].position.set(0.0, (currentFrames[i] - 1.0) / 2, 2 + 1.5 * size * i);
+            }
+            //Load new pages
+        }
+        else {
+            console.log("Not Full")
+            while (currentFrames[index] > 0.95) {
+                index = Math.floor(Math.random() * (options.frames - 1));
+            }
+            currentFrames[index] += 0.1
+            currentFramesObject.children[index].scale.set(1, currentFrames[index], 1)
+            currentFramesObject.children[index].position.set(0.0, (currentFrames[index] - 1.0) / 2, 2 + 1.5 * size * index);
+            createRuns()
+        }
+    }, 150)
 }
 function updateRuns(index) {
     currentFramesObject.children[index].scale.set(1, currentFrames[index], 1)
@@ -317,18 +391,18 @@ function movePagesToFrame(callback) {
     for (let i = 0; i < options.frames; i++) {
         currentFrames.push(0)
     }
-    for (let i = 0; i < options.frames - 1 && i < options.pages; i++) {
+    for (let i = 0; i < options.frames && i < options.pages; i++) {
         tweens.push(new TWEEN.Tween(relation.children[relation.children.length - 1 - i].position, tweenGroup).to(new THREE.Vector3(frames.children[i].position.x, relation.children[relation.children.length - 1 - i].position.y, 0), time))
     }
-    for (let i = 0; i < options.frames - 1 && i < options.pages; i++) {
+    for (let i = 0; i < options.frames && i < options.pages; i++) {
         if (i < tweens.length - 1) {
             tweens[i].chain(tweens[i + 1])
         }
         tweens[i].onComplete(() => {
-            let tween = new TWEEN.Tween(relation.children[relation.children.length - 1 - i].position, tweenGroup).to(new THREE.Vector3(frames.children[frames.children.length - 2 - i].position.x, relation.children[relation.children.length - 1 - i].position.y, frames.children[frames.children.length - 2 - i].position.z), time)
+            let tween = new TWEEN.Tween(relation.children[relation.children.length - 1 - i].position, tweenGroup).to(new THREE.Vector3(frames.children[frames.children.length - 1 - i].position.x, relation.children[relation.children.length - 1 - i].position.y, frames.children[frames.children.length - 1 - i].position.z), time)
             tween.onComplete(() => {
-                let tween2 = new TWEEN.Tween(relation.children[relation.children.length - 1 - i].position, tweenGroup).to(frames.children[frames.children.length - 2 - i].position, time)
-                if (!((i + 1) < options.frames - 1 && (i + 1) < options.pages)) {
+                let tween2 = new TWEEN.Tween(relation.children[relation.children.length - 1 - i].position, tweenGroup).to(frames.children[frames.children.length - 1 - i].position, time)
+                if (!((i + 1) < options.frames && (i + 1) < options.pages)) {
                     tween2.onComplete(() => {
                         callback()
                     })
